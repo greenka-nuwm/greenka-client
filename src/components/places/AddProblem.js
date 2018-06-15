62fb0222b8cb7640b408110ae3a8e12242ae1f68
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2018-06-16 00:18:34 +0300

    Add loader to create problem page

--- src/components/places/AddProblem.js
+++ src/components/places/AddProblem.js
@@ -1,9 +1,10 @@
 import React, { Fragment, Component } from 'react';
 import PropTypes from 'prop-types';
-import { AsyncStorage, ScrollView, StatusBar, View } from 'react-native';
+import { ActivityIndicator, Alert, AsyncStorage, ScrollView, StatusBar, View } from 'react-native';
 import { Dropdown } from 'react-native-material-dropdown';
 import { TextField } from 'react-native-material-textfield';
 import { COLOR, ThemeProvider, Toolbar } from 'react-native-material-ui';
+import Snackbar from 'react-native-snackbar';
 import { formContainer, uiTheme } from '../../consts/styles';
 import { LOCATION } from '../../consts/appConsts';
 import NavigationService from '../../services/NavigationService';
@@ -16,10 +17,11 @@ class AddProblem extends Component {
 
     this.state = {
       name: this.props.name,
-      address: this.props.address,
+      addressString: this.props.addressString,
+      location: this.props.location,
       type: this.props.type,
       description: this.props.description,
-      types: [],
+      isDataFetched: false,
       showNameError: false,
       showAddressError: false,
       showTypeError: false,
@@ -30,44 +32,14 @@ class AddProblem extends Component {
     const location = JSON.parse(await AsyncStorage.getItem('location'));
     const types = await ProblemsService.getProblemsTypes();
 
-    this.setState({ address: { location }, types });
-  }
-
-  handleSubmit() {
-    const showNameError = this.state.name === '';
-    const showAddressError = this.state.address.addressString === '';
-    const showTypeError = this.state.type.value === '';
-
-    this.setState({ showNameError, showAddressError, showTypeError });
-
-    if (!(showNameError || showAddressError || showTypeError)) {
-      const problem = {
-      };
-
-      ProblemsService.create(problem);
-    }
-  }
-
-  handleNameChange(name) {
-    this.setState({ name, showNameError: false });
-  }
-
-  handleAddressChange(address) {
-    this.setState({ address, showAddressError: false });
-  }
-
-  handleTypeChange(value, index) {
     this.setState({
-      type: { id: index, value },
-      showTypeError: false,
+      location,
+      types,
+      isDataFetched: true,
     });
   }
 
-  handleDescriptionChange(description) {
-    this.setState({ description });
-  }
-
-  render() {
+  getPage() {
     return (
       <ThemeProvider uiTheme={uiTheme}>
         <Fragment>
@@ -101,7 +73,8 @@ class AddProblem extends Component {
             </View>
 
             <AddressField
-              address={this.state.address}
+              addressString={this.state.addressString}
+              location={this.state.location}
               showAddressError={this.state.showAddressError}
               onAddressChange={this.handleAddressChange}
             />
@@ -129,18 +102,104 @@ class AddProblem extends Component {
       </ThemeProvider>
     );
   }
+
+  handleSubmit = async () => {
+    const showNameError = this.state.name === '';
+    const showAddressError = this.state.addressString === '';
+    const showTypeError = this.state.type.value === '';
+
+    this.setState({ showNameError, showAddressError, showTypeError });
+
+    if (!(showNameError || showAddressError || showTypeError)) {
+      const problem = {
+        verbose_name: this.state.name,
+        name: this.state.name,
+        latitude: this.state.location.latitude,
+        longitude: this.state.location.longitude,
+        problem_type: this.state.type.id,
+      };
+
+      if (this.state.description) {
+        problem.description = this.state.description;
+      }
+
+      try {
+        await ProblemsService.create(problem);
+
+        this.setState({
+          name: this.props.name,
+          addressString: this.props.addressString,
+          location: this.state.userLocation,
+          type: this.props.type,
+          description: this.props.description,
+          showNameError: false,
+          showAddressError: false,
+          showTypeError: false,
+        });
+
+        Snackbar.show({
+          title: 'Проблему внесено',
+          duration: Snackbar.LENGTH_SHORT,
+        });
+      } catch (e) {
+        Alert.alert(
+          '',
+          'Не вдалось внести проблему',
+          [
+            {
+              text: 'Ок',
+              style: 'cancel',
+            },
+          ],
+        );
+      }
+    }
+  };
+
+  handleNameChange = name => {
+    this.setState({ name, showNameError: false });
+  };
+
+  handleAddressChange = address => {
+    this.setState({
+      addressString: address.addressString,
+      location: address.location,
+      showAddressError: false,
+    });
+  };
+
+  handleTypeChange = (value, index) => {
+    this.setState({
+      type: { id: index, value },
+      showTypeError: false,
+    });
+  };
+
+  handleDescriptionChange = description => {
+    this.setState({ description });
+  };
+
+  render() {
+    return (
+      this.state.isDataFetched
+        ? this.getPage()
+        : (
+          <View style={{ flex: 1, justifyContent: 'center' }}>
+            <ActivityIndicator size="large" color={uiTheme.palette.primaryColor} />
+          </View>
+        )
+    );
+  }
 }
 
 AddProblem.propTypes = {
   name: PropTypes.string,
-  address: PropTypes.shape({
-    addressString: PropTypes.string,
-    location: PropTypes.shape({
-      latitude: PropTypes.number,
-      longitude: PropTypes.number,
-      latitudeDelta: PropTypes.number,
-      longitudeDelta: PropTypes.number,
-    }),
+  addressString: PropTypes.string,
+  location: PropTypes.shape({
+    latitude: PropTypes.number,
+    longitude: PropTypes.number,
+    latitudeDelta: PropTypes.number,
+    longitudeDelta: PropTypes.number,
   }),
   type: PropTypes.shape({
     id: PropTypes.number,
@@ -151,7 +210,8 @@ AddProblem.propTypes = {
 
 AddProblem.defaultProps = {
   name: '',
-  address: { addressString: '', location: LOCATION },
+  addressString: '',
+  location: LOCATION,
   type: { id: 0, value: '' },
   description: '',
 };
--- src/services/ProblemsService.js
+++ src/services/ProblemsService.js
@@ -1,6 +1,17 @@
+import axios from 'axios';
+
 // TODO: error handling
 
 class ProblemsService {
+  static async getProblemsTypes() {
+    return (await axios.get('/problems/types/')).data
+      .map(type => ({ id: type.id, value: type.verbose_name }));
+  }
+
+  static async create(problem) {
+    return axios
+      .post('/problems/', problem);
+  }
 }
 
 export default ProblemsService;
